769ea55d79eb87f5700ecb4274a65e915c7f8b53
susme2020 <s.usme@uniandes.edu.co>
2020-03-01 21:49:32 -0500

    Reto 2, falta la aplicación

--- App/view.py
+++ App/view.py
@@ -82,6 +82,7 @@ def loadData (catalog):
 """
 Menu principal
 """
+datos_cargados = False
 while True:
     printMenu()
     inputs =input('Seleccione una opción para continuar\n')
@@ -99,44 +100,75 @@ while True:
         print ('Mapa: Actores cargados: ' + str(map.size(catalog['actors'])))
         print ('Mapa: Directores cargados: ' + str(map.size(catalog['directors'])))
         print ('Mapa: Géneros cargados: ' + str(map.size(catalog['genres'])))
+
+        datos_cargados = True
         
     elif int(inputs[0])==2: # 2- Buscar película por título
-        """
-        bookTitle = input("Nombre del libro a buscar: ")
-        book = controller.getBookInfo (catalog, bookTitle)
-        if book:
-            print("Libro encontrado:",book['title'],",Rating:",book['average_rating'])
+        if not datos_cargados:
+            """
+            bookTitle = input("Nombre del libro a buscar: ")
+            book = controller.getBookInfo (catalog, bookTitle)
+            if book:
+                print("Libro encontrado:",book['title'],",Rating:",book['average_rating'])
+            else:
+                print("Libro No encontrado")
+            """
+            movieTitle = input("Nombre de la película a buscar: ")
+            movie = controller.getMovieInfo (catalog, movieTitle)
+            if movie:
+                print("Película encontrada:", movie['title'], ", ID:", movie['id'], "\nVoto Promedio: ",  movie['vote_average'], "\nDirector: ", movie['director'], "\nActores: ", movie["actors"])
+            else:
+                print("Película No encontrada")
         else:
-            print("Libro No encontrado")
-        """
-        movieTitle = input("Nombre de la película a buscar: ")
-        movie = controller.getMovieInfo (catalog, movieTitle)
-        if movie:
-            print("Película encontrada:", movie['title'], ", ID:", movie['id'], "\nVoto Promedio: ",  movie['vote_average'], "\nDirector: ", movie['director'], "\nActores: ", movie["actors"])
-        else:
-            print("Película No encontrada")
+            print("No ha cargado los datos aún")
 
     elif int(inputs[0])==3: # 3- Buscar información de director por nombre ...
-        """
-        authorName = input("Nombre del autor a buscar: ")
-        author = controller.getAuthorInfo (catalog, authorName)
-        if author:
-            print("Libros del autor",authorName,":",lt.size(author['authorBooks']))
-            print("Promedio de Votación: ",authorName,(author['sum_average_rating']/lt.size(author['authorBooks'])))
+        if not datos_cargados:
+            """
+            authorName = input("Nombre del autor a buscar: ")
+            author = controller.getAuthorInfo (catalog, authorName)
+            if author:
+                print("Libros del autor",authorName,":",lt.size(author['authorBooks']))
+                print("Promedio de Votación: ",authorName,(author['sum_average_rating']/lt.size(author['authorBooks'])))
+            else:
+                print("Autor No encontrado")
+            """
+            director_name = input("Nombre del director a buscar: ")
+            director = controller.getDirectorInfo (catalog, director_name)
+            if director:
+                print("Películas dirigidas por ", director_name,": ", lt.size(director['movie_titles']))
+                print("Promedio de Votación: ", (director['sum_average_rating']/lt.size(director['movie_titles'])))
+            else:
+                print("Director No encontrado")
         else:
-            print("Autor No encontrado")
-        """
-        director_name = input("Nombre del director a buscar: ")
-        director = controller.getDirectorInfo (catalog, director_name)
-        if director:
-            print("Películas dirigidas por ", director_name,": ", lt.size(director['movie_titles']))
-            print("Promedio de Votación: ", (director['sum_average_rating']/lt.size(director['movie_titles'])))
+            print("No ha cargado los datos aún")
+
+    elif int(inputs[0])==4: # 4- Buenas películas, cuantas películas con votación positiva (vote_average ≥ 6) tiene dado un nombre exacto de director
+        if not datos_cargados:
+            label = input (" ")
         else:
-            print("Director No encontrado") 
-        
-    elif int(inputs[0])==4:
-        label = input (" ")
-        pass
+            print("No ha cargado los datos aún")
+
+    elif int(inputs[0])==5: # 5- Votos, los datos de una película por título exacto: voto promedio, votos totales y su director
+        if not datos_cargados:
+        else:
+            print("No ha cargado los datos aún")
+
+    elif int(inputs[0])==6: # 6- Directores, todas las películas de un director dado su nombre exacto. Indicando el número de películas dirigidas, y el promedio de votos obtenidos en sus películas.
+        if not datos_cargados:
+        else:
+            print("No ha cargado los datos aún")
+
+    elif int(inputs[0])==7: # 7- Actores, todas las películas en las que ha participado un actor dado su nombre exacto. Indicando el total de películas en las que ha participado, el promedio de votos de sus películas y el director que más veces lo ha dirigido.
+        if not datos_cargados:
+        else:
+            print("No ha cargado los datos aún")
+
+    elif int(inputs[0])==8: # 8- Géneros, cuántas películas tiene asociadas un género (genres) a partir del nombre exacto del género. Adicionalmente se debe indicar para dicho género, el promedio de votos de todas las películas de dicho género
+        if not datos_cargados:
+        else:
+            print("No ha cargado los datos aún")
+
     else:
         sys.exit(0)
 sys.exit(0)
